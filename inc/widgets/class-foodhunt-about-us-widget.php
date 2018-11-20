f7a310fa5285352638884c915542da1d029f5fd5
razyees <rajeshpoudel9849@gmail.com>
2018-11-20 15:04:48 +0545

    Add - Alt text for service widget

--- inc/widgets/class-foodhunt-about-us-widget.php
+++ inc/widgets/class-foodhunt-about-us-widget.php
@@ -119,9 +119,9 @@ class foodhunt_about_us_widget extends WP_Widget {
 
 							if( has_post_thumbnail() ) { ?>
 								<?php $title_attribute = the_title_attribute( 'echo=0' );
-								$thumb_id              = get_post_thumbnail_id( get_the_ID() );
-								$img_altr              = get_post_meta( $thumb_id, '_wp_attachment_image_alt', true );
-								$img_alt               = ! empty( $img_altr ) ? $img_altr : $title_attribute;
+								$thumb_id            = get_post_thumbnail_id( get_the_ID() );
+								$img_altr            = get_post_meta( $thumb_id, '_wp_attachment_image_alt', true );
+								$img_alt             = ! empty( $img_altr ) ? $img_altr : $title_attribute;
 								$post_thumbnail_attr = array(
 									'alt'   => esc_attr( $img_alt ),
 									'title' => esc_attr( $title_attribute ),
